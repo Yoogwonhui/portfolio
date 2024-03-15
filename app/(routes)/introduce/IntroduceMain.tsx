74b2a9c6bbe5d88be25b46c3001ffe6ba554a7bf
yoogwonhee <dbrnjsgml000@naver.com>
2024-03-15 15:03:15 +0900

    Feat : TypeAnimation 추가

--- app/(routes)/introduce/IntroduceMain.tsx
+++ app/(routes)/introduce/IntroduceMain.tsx
@@ -1,9 +1,24 @@
 import { introduceData } from '@/app/_lib/data';
 import IntroduceCard from '@/app/_components/IntroduceCard';
+import TypeAnimationComponent from '@/app/_components/TypeAnimationComponent';
 
 export default function IntroduceMain() {
 	return (
 		<div className="flex flex-col items-center">
+			<TypeAnimationComponent
+				sequenceList={[
+					'꾸준히 성장하는',
+					1000,
+					'문제 해결에 열정적인',
+					1000,
+					'사용자 중심의',
+					1000,
+					'협업을 중시하는',
+					1000,
+				]}
+				style={{ marginBottom: 30, marginTop: 20 }}
+			/>
+
 			{introduceData.introduceList.map((d, index) => (
 				<div key={index} className="text-lg ">
 					{d}
---
+++ app/_components/TypeAnimationComponent.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { TypeAnimation } from 'react-type-animation';
+
+export default function TypeAnimationComponent({
+	sequenceList,
+	style,
+}: {
+	sequenceList: (string | number)[];
+	style?: React.CSSProperties;
+}) {
+	return (
+		<div className="flex text-2xl md:text-4xl">
+			<TypeAnimation
+				sequence={sequenceList}
+				wrapper="span"
+				speed={10}
+				style={{
+					display: 'inline-block',
+					fontWeight: 'bold',
+					...style,
+				}}
+				repeat={Infinity}
+			/>
+			<div
+				style={{
+					display: 'inline-block',
+					fontWeight: 'bold',
+					...style,
+				}}
+			>
+				프론트엔드 개발자
+			</div>
+		</div>
+	);
+}
